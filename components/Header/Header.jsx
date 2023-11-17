9ace8c222a8a21b5d93bfb68d15ba96b41257a22
Ephraim Siegfried <e.siegfried@stud.unibas.ch>
2023-11-17 20:43:53 +0100

    initial commit

---
+++ components/Header/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./Header.css";
+import logo from "../../assets/logo.png";
+
+export default function Header() {
+  return (
+    <header>
+      <h1>Cuisinventory</h1>
+    </header>
+  );
+}
