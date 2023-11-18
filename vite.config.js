ade748a69575b60a9443aafc18825ae1682b48d1
Ephraim Siegfried <e.siegfried@stud.unibas.ch>
2023-11-18 14:34:27 +0100

    another try

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/cuisinventory-web/",
+  base: "https://ephraimsiegfried.github.io/cuisinventory-web/",
 });
