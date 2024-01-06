2c37e0e6df13a879553526f952ee17dbf2594333
Ephraim Siegfried <e.siegfried@stud.unibas.ch>
2024-01-06 20:13:45 +0100

    change theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,6 @@ module.exports = {
   },
   plugins: [require("daisyui")],
   daisyui: {
-    themes: ["light" /* , "dark", "retro", "garden" */],
+    themes: [/* "light", "dark", */ "retro" /* , "garden" */],
   },
 };
